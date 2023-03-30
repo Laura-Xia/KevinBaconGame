e16de03b4af0a49b2bad3c24c54c4fe4b8c2ef44
Laura-Xia <95654828+Laura-Xia@users.noreply.github.com>
2023-03-30 08:08:30 -0400

    Update BFS.java

--- ClassworkHomework - IV Form/src/BFS.java
+++ ClassworkHomework - IV Form/src/BFS.java
@@ -247,16 +247,18 @@ public class BFS{
             }
         }
         if(yay) {
-        	int num = 0;
+            int num = 0;
             Stack<Nodes> backtrack = new Stack<Nodes>();
+	    Stack<Nodes> dup = new Stack<Nodes>();
             Queue<String> movies = new LinkedList<String>();
             Nodes target = name_node.get(end);
             backtrack.add(target);
+	    dup.add(target);
             while (!target.getName().equals(k)) {
                 target = target.getPrev();
                 backtrack.add(target);
+		dup.add(target);
             }
-            Stack<Nodes> dup = backtrack;
             Nodes first = s;
             Nodes second = dup.pop();
             while(dup.size() > 0){
